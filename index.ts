213132307a7674a56bbc87fc49ca827e336e134f
mmattbtw <matt@mmatt.net>
2025-06-19 11:52:35 -0500

    rewrite for backlink method

--- index.ts
+++ index.ts
@@ -17,7 +17,42 @@ const CNN_VERIFIED_LIST = "3lpsin5ibqc2p";
 const LA_TIMES_VERIFIED_LIST = "3lpsints3js2p";
 const IGN_VERIFIED_LIST = "3lrdq5yralk2v";
 
+const VERIFIED_LISTS = {
+  "did:plc:z72i7hdynmk6r22z27h6tvur": BSKY_VERIFIED_LIST,
+  "did:plc:b2kutgxqlltwc6lhs724cfwr": THE_ATHLETIC_VERIFIED_LIST,
+  "did:plc:inz4fkbbp7ms3ixufw6xuvdi": WIRED_VERIFIED_LIST,
+  "did:plc:eclio37ymobqex2ncko63h4r": NYT_VERIFIED_LIST,
+  "did:plc:wmho6q2uiyktkam3jsvrms3s": NBC_VERIFIED_LIST,
+  "did:plc:sqbswn3lalcc2dlh2k7zdpuw": YAHOO_FINANCE_VERIFIED_LIST,
+  "did:plc:y3xrmnwvkvsq4tqcsgwch4na": GLOBAL_MAIL_VERIFIED_LIST,
+  "did:plc:d2jith367s6ybc3ldsusgdae": LA_TIMES_VERIFIED_LIST,
+  "did:plc:dzezcmpb3fhcpns4n4xm4ur5": CNN_VERIFIED_LIST,
+  "did:plc:xwqgusybtrpm67tcwqdfmzvy": IGN_VERIFIED_LIST,
+} as Record<`did:${string}`, string>;
+
 const ALL_VERIFIED_LIST = "3lngcmewutk2z";
+
+const BSKY_DID = "did:plc:z72i7hdynmk6r22z27h6tvur";
+
+type BacklinkResponse = {
+  total: number;
+  linking_dids: string[];
+  cursor?: string;
+};
+
+// should probably just make this redis  but whateva
+let handleResolutions = new Map<`did:${string}`, string>();
+
+async function resolveDID(did: `did:${string}`): Promise<string> {
+  if (handleResolutions.has(did)) {
+    console.log(":) CACHE HIT ", did);
+    return handleResolutions.get(did) as string;
+  }
+  console.log(":( CACHE MISS ", did);
+  const handle = (await bot.getProfile(did)).handle;
+  handleResolutions.set(did, handle);
+  return handle;
+}
 
 const bot = new Bot({
   service: process.env.BSKY_PDS,
@@ -47,68 +82,26 @@ jetstream.onCreate("app.bsky.graph.verification", async (event) => {
   // Save the current cursor to file
   writeFileSync("cursor.txt", event.time_us.toString());
 
-  // Bluesky
-  if (event.did === "did:plc:z72i7hdynmk6r22z27h6tvur") {
-    const richText = new RichText()
-      .addText("✅ ")
-      // @ts-ignore
-      .addMention(`@${event.commit.record.handle}`, event.commit.record.subject)
-      .addText(" has been verified by Bluesky.");
-    await bot.post({
-      text: richText,
-    });
+  const backlinks = (await (
+    await fetch(
+      `https://constellation.microcosm.blue/links/distinct-dids?target=${event.did}&collection=app.bsky.graph.verification&path=.subject`
+    )
+  ).json()) as BacklinkResponse;
 
-    // All Verified Accounts List
-    await bot.createRecord("app.bsky.graph.listitem", {
-      list: `at://did:plc:k3lft27u2pjqp2ptidkne7xr/app.bsky.graph.list/${ALL_VERIFIED_LIST}`,
-      // @ts-ignore
-      subject: event.commit.record.subject,
-    });
-    // Bluesky Verified Accounts List
-    await bot.createRecord("app.bsky.graph.listitem", {
-      list: `at://did:plc:k3lft27u2pjqp2ptidkne7xr/app.bsky.graph.list/${BSKY_VERIFIED_LIST}`,
-      // @ts-ignore
-      subject: event.commit.record.subject,
-    });
+  if (backlinks.linking_dids.length == 0 && !(event.did == BSKY_DID)) {
+    console.log("No backlinks found, skipping verification.");
+    return;
   }
-  // The Athletic
-  if (event.did === "did:plc:b2kutgxqlltwc6lhs724cfwr") {
-    const richText = new RichText()
-      .addText("✅ ")
-      // @ts-ignore
-      .addMention(`@${event.commit.record.handle}`, event.commit.record.subject)
-      .addText(" has been verified by ")
-      .addMention(
-        // TODO: probably don't hardcode this handle.
-        `@theathletic.bsky.social`,
-        "did:plc:b2kutgxqlltwc6lhs724cfwr"
-      )
-      .addText(".");
-    await bot.post({
-      text: richText,
-    });
 
-    // All Verified Accounts List
-    await bot.createRecord("app.bsky.graph.listitem", {
-      list: `at://did:plc:k3lft27u2pjqp2ptidkne7xr/app.bsky.graph.list/${ALL_VERIFIED_LIST}`,
-      // @ts-ignore
-      subject: event.commit.record.subject,
-    });
-    // The Athletic Verified Accounts List
-    await bot.createRecord("app.bsky.graph.listitem", {
-      list: `at://did:plc:k3lft27u2pjqp2ptidkne7xr/app.bsky.graph.list/${THE_ATHLETIC_VERIFIED_LIST}`,
-      // @ts-ignore
-      subject: event.commit.record.subject,
-    });
-  }
-  // WIRED
-  if (event.did === "did:plc:inz4fkbbp7ms3ixufw6xuvdi") {
+  if (backlinks.linking_dids.includes(BSKY_DID) || event.did == BSKY_DID) {
     const richText = new RichText()
       .addText("✅ ")
       // @ts-ignore
       .addMention(`@${event.commit.record.handle}`, event.commit.record.subject)
+      // .addText(`@${event.commit.record.handle}`)
       .addText(" has been verified by ")
-      .addMention(`@wired.com`, "did:plc:inz4fkbbp7ms3ixufw6xuvdi")
+      .addMention(`@${await resolveDID(event.did)}`, event.did)
+      // .addText(`@${await resolveDID(event.did)}`)
       .addText(".");
     await bot.post({
       text: richText,
@@ -121,201 +114,23 @@ jetstream.onCreate("app.bsky.graph.verification", async (event) => {
       subject: event.commit.record.subject,
     });
 
-    // Wired Verified Accounts List
-    await bot.createRecord("app.bsky.graph.listitem", {
-      list: `at://did:plc:k3lft27u2pjqp2ptidkne7xr/app.bsky.graph.list/${WIRED_VERIFIED_LIST}`,
-      // @ts-ignore
-      subject: event.commit.record.subject,
-    });
-  }
-  // NYT
-  if (event.did === "did:plc:eclio37ymobqex2ncko63h4r") {
-    const richText = new RichText()
-      .addText("✅ ")
-      // @ts-ignore
-      .addMention(`@${event.commit.record.handle}`, event.commit.record.subject)
-      .addText(" has been verified by ")
-      .addMention(`@nytimes.com`, "did:plc:eclio37ymobqex2ncko63h4r")
-      .addText(".");
-    await bot.post({
-      text: richText,
-    });
-
-    // All Verified Accounts List
-    await bot.createRecord("app.bsky.graph.listitem", {
-      list: `at://did:plc:k3lft27u2pjqp2ptidkne7xr/app.bsky.graph.list/${ALL_VERIFIED_LIST}`,
-      // @ts-ignore
-      subject: event.commit.record.subject,
-    });
-
-    // NYT Verified Accounts List
-    await bot.createRecord("app.bsky.graph.listitem", {
-      list: `at://did:plc:k3lft27u2pjqp2ptidkne7xr/app.bsky.graph.list/${NYT_VERIFIED_LIST}`,
-      // @ts-ignore
-      subject: event.commit.record.subject,
-    });
-  }
-  // NBC
-  if (event.did === "did:plc:wmho6q2uiyktkam3jsvrms3s") {
-    const richText = new RichText()
-      .addText("✅ ")
-      // @ts-ignore
-      .addMention(`@${event.commit.record.handle}`, event.commit.record.subject)
-      .addText(" has been verified by ")
-      .addMention(`@nbcnews.com`, "did:plc:wmho6q2uiyktkam3jsvrms3s")
-      .addText(".");
-    await bot.post({
-      text: richText,
-    });
-
-    // All Verified Accounts List
-    await bot.createRecord("app.bsky.graph.listitem", {
-      list: `at://did:plc:k3lft27u2pjqp2ptidkne7xr/app.bsky.graph.list/${ALL_VERIFIED_LIST}`,
-      // @ts-ignore
-      subject: event.commit.record.subject,
-    });
-
-    // NBC Verified Accounts List
-    await bot.createRecord("app.bsky.graph.listitem", {
-      list: `at://did:plc:k3lft27u2pjqp2ptidkne7xr/app.bsky.graph.list/${NBC_VERIFIED_LIST}`,
-      // @ts-ignore
-      subject: event.commit.record.subject,
-    });
-  }
-  // Yahoo Finance
-  if (event.did === "did:plc:sqbswn3lalcc2dlh2k7zdpuw") {
-    const richText = new RichText()
-      .addText("✅ ")
-      // @ts-ignore
-      .addMention(`@${event.commit.record.handle}`, event.commit.record.subject)
-      .addText(" has been verified by ")
-      .addMention(`@yahoofinance.com`, "did:plc:sqbswn3lalcc2dlh2k7zdpuw")
-      .addText(".");
-    await bot.post({
-      text: richText,
-    });
-
-    // All Verified Accounts List
-    await bot.createRecord("app.bsky.graph.listitem", {
-      list: `at://did:plc:k3lft27u2pjqp2ptidkne7xr/app.bsky.graph.list/${ALL_VERIFIED_LIST}`,
-      // @ts-ignore
-      subject: event.commit.record.subject,
-    });
-
-    // Yahoo Finance Verified Accounts List
-    await bot.createRecord("app.bsky.graph.listitem", {
-      list: `at://did:plc:k3lft27u2pjqp2ptidkne7xr/app.bsky.graph.list/${YAHOO_FINANCE_VERIFIED_LIST}`,
-      // @ts-ignore
-      subject: event.commit.record.subject,
-    });
-  }
-  // The Globe and Mail
-  if (event.did === "did:plc:y3xrmnwvkvsq4tqcsgwch4na") {
-    const richText = new RichText()
-      .addText("✅ ")
-      // @ts-ignore
-      .addMention(`@${event.commit.record.handle}`, event.commit.record.subject)
-      .addText(" has been verified by ")
-      .addMention(`@theglobeandmail.com`, "did:plc:y3xrmnwvkvsq4tqcsgwch4na")
-      .addText(".");
-    await bot.post({
-      text: richText,
-    });
-
-    // All Verified Accounts List
-    await bot.createRecord("app.bsky.graph.listitem", {
-      list: `at://did:plc:k3lft27u2pjqp2ptidkne7xr/app.bsky.graph.list/${ALL_VERIFIED_LIST}`,
-      // @ts-ignore
-      subject: event.commit.record.subject,
-    });
-
-    // The Globe and Mail Verified Accounts List
-    await bot.createRecord("app.bsky.graph.listitem", {
-      list: `at://did:plc:k3lft27u2pjqp2ptidkne7xr/app.bsky.graph.list/${GLOBAL_MAIL_VERIFIED_LIST}`,
-      // @ts-ignore
-      subject: event.commit.record.subject,
-    });
-  }
-  // LA Times
-  if (event.did === "did:plc:d2jith367s6ybc3ldsusgdae") {
-    const richText = new RichText()
-      .addText("✅ ")
-      // @ts-ignore
-      .addMention(`@${event.commit.record.handle}`, event.commit.record.subject)
-      .addText(" has been verified by ")
-      .addMention(`@latimes.com`, "did:plc:d2jith367s6ybc3ldsusgdae")
-      .addText(".");
-    await bot.post({
-      text: richText,
-    });
-
-    // All Verified Accounts List
-    await bot.createRecord("app.bsky.graph.listitem", {
-      list: `at://did:plc:k3lft27u2pjqp2ptidkne7xr/app.bsky.graph.list/${ALL_VERIFIED_LIST}`,
-      // @ts-ignore
-      subject: event.commit.record.subject,
-    });
-
-    // LA Times Verified Accounts List
-    await bot.createRecord("app.bsky.graph.listitem", {
-      list: `at://did:plc:k3lft27u2pjqp2ptidkne7xr/app.bsky.graph.list/${LA_TIMES_VERIFIED_LIST}`,
-      // @ts-ignore
-      subject: event.commit.record.subject,
-    });
-  }
-  // CNN
-  if (event.did === "did:plc:dzezcmpb3fhcpns4n4xm4ur5") {
-    const richText = new RichText()
-      .addText("✅ ")
-      // @ts-ignore
-      .addMention(`@${event.commit.record.handle}`, event.commit.record.subject)
-      .addText(" has been verified by ")
-      .addMention(`@cnn.com`, "did:plc:dzezcmpb3fhcpns4n4xm4ur5")
-      .addText(".");
-    await bot.post({
-      text: richText,
-    });
-
-    // All Verified Accounts List
-    await bot.createRecord("app.bsky.graph.listitem", {
-      list: `at://did:plc:k3lft27u2pjqp2ptidkne7xr/app.bsky.graph.list/${ALL_VERIFIED_LIST}`,
-      // @ts-ignore
-      subject: event.commit.record.subject,
-    });
-
-    // CNN Verified Accounts List
-    await bot.createRecord("app.bsky.graph.listitem", {
-      list: `at://did:plc:k3lft27u2pjqp2ptidkne7xr/app.bsky.graph.list/${CNN_VERIFIED_LIST}`,
-      // @ts-ignore
-      subject: event.commit.record.subject,
-    });
-  }
-
-  // IGN
-  if (event.did === "did:plc:xwqgusybtrpm67tcwqdfmzvy") {
-    const richText = new RichText()
-      .addText("✅ ")
-      // @ts-ignore
-      .addMention(`@${event.commit.record.handle}`, event.commit.record.subject)
-      .addText(" has been verified by ")
-      .addMention(`@ign.com`, "did:plc:xwqgusybtrpm67tcwqdfmzvy")
-      .addText(".");
-    await bot.post({
-      text: richText,
-    });
-
-    // All Verified Accounts List
-    await bot.createRecord("app.bsky.graph.listitem", {
-      list: `at://did:plc:k3lft27u2pjqp2ptidkne7xr/app.bsky.graph.list/${ALL_VERIFIED_LIST}`,
-      // @ts-ignore
-      subject: event.commit.record.subject,
-    });
-
-    // IGN Verified Accounts List
-    await bot.createRecord("app.bsky.graph.listitem", {
-      list: `at://did:plc:k3lft27u2pjqp2ptidkne7xr/app.bsky.graph.list/${IGN_VERIFIED_LIST}`,
-      // @ts-ignore
-      subject: event.commit.record.subject,
-    });
+    const verifiedList = VERIFIED_LISTS[event.did];
+    if (verifiedList) {
+      await bot.createRecord("app.bsky.graph.listitem", {
+        list: `at://did:plc:k3lft27u2pjqp2ptidkne7xr/app.bsky.graph.list/${verifiedList}`,
+        // @ts-ignore
+        subject: event.commit.record.subject,
+      });
+    } else {
+      console.log(`No verified list found for DID: ${event.did}`);
+      const convoID = await bot.getConversationForMembers([
+        "did:plc:tas6hj2xjrqben5653v5kohk",
+      ]);
+      await convoID.sendMessage({
+        text: new RichText()
+          .addText("No verified list found for ")
+          .addMention(`@${event.did}`, event.did),
+      });
+    }
   }
 });
